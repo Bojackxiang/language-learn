077c15eb6b31466065685f4f5036da1e8e71bc25
Alex <alex@q-83.com>
2024-03-27 21:45:00 +1100

    update card layout in the challenge

--- src/app/(main)/lesson/quiz.tsx
+++ src/app/(main)/lesson/quiz.tsx
@@ -1,8 +1,10 @@
-"user client"
+"use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { challengeOptions, challenges, userSubscription } from '../../../../db/schema';
 import Header from './header';
+import QuestionBubble from './question-bubble';
+import Challenge from './challenge';
 
 interface Props {
   initialPercentage: number;
@@ -24,6 +26,26 @@ const Quiz = ({
   initialLessonChallenges,
   userSubscription
 }: Props) => {
+
+  const [lessonId] = useState(initialLessonId);
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage;
+  });
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+
+  const title = challenge.type === "ASSIST"
+    ? "Select the correct meaning"
+    : challenge.question;
+
   return (
     <>
       <Header
@@ -31,6 +53,41 @@ const Quiz = ({
         percentage={initialPercentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "SELECT" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => { }}
+                status={"none"}
+                selectedOption={1}
+                disabled={false}
+                type={challenge.type}
+              />
+              {/* <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={pending}
+                type={challenge.type}
+              /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Footer
+        disabled={pending || !selectedOption}
+        status={status}
+        onCheck={onContinue}
+      /> */}
     </>
   )
 }
